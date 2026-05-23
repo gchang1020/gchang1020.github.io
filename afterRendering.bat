7e5485470f565befd30890f6cccae5068687ade1
gchang1020 <gchang1020@yahoo.com>
2026-05-23 18:10:06 -0500

    should be much finer

--- afterRendering.bat
+++ afterRendering.bat
@@ -1,17 +1,19 @@
 @echo off
 
-:: insert '<br>'
-set HTML=d:\workspace\gchang1020.github.io\certificates.html
-sed -e "s#Sciences</strong>#Sciences</strong><br>#g" -e "s#Certificate</strong>#Certificate</strong><br>#g" "%HTML%" > "%HTML%.tmp"
-move /Y "%HTML%.tmp" "%HTML%"
+:: insert '<br>' in certificates.html
+echo.Insert <BR>
+REM set HTML=d:\workspace\gchang1020.github.io\certificates.html
+REM sed -e "s#...#...<br>#g" "%HTML%" > "%HTML%.tmp"
+REM move /Y "%HTML%.tmp" "%HTML%"
 
 :: copy cv.pdf
+echo.Update CV.pdf
 copy /Y "D:\Gary\DmGc\Jobs\res-utd-Industry.pdf" "d:\workspace\gchang1020.github.io\cv.pdf"
 
 echo.Done!
 
 echo.
-echo.You should run:
+echo.Next, you should run:
 echo.- git add.
 echo.- git commit -m "message"
 echo.- ssh git@github.com (if not login yet)
