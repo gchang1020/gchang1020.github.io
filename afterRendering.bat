be7bc9c1da5e6e8a8a18e990996ce2baae9459f9
gchang1020 <gchang1020@yahoo.com>
2026-05-23 16:56:02 -0500

    further improvement

---
+++ afterRendering.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: insert '<br>'
+set HTML=d:\workspace\gchang1020.github.io\certificates.html
+sed -e "s#Sciences</strong>#Sciences</strong><br>#g" -e "s#Certificate</strong>#Certificate</strong><br>#g" "%HTML%" > "%HTML%.tmp"
+move /Y "%HTML%.tmp" "%HTML%"
+
+:: copy cv.pdf
+copy /Y "D:\Gary\DmGc\Jobs\res-utd-Industry.pdf" "d:\workspace\gchang1020.github.io\cv.pdf"
+
+echo Done
